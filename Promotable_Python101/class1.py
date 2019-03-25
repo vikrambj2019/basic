83fe6b2b11493d8736fcc438b6cfed4bd3086e63
vikrambj2019 <46285637+vikrambj2019@users.noreply.github.com>
2019-03-25 18:29:04 -0500

    Add files via upload

---
+++ Promotable_Python101/class1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 20:27:24 2019
+
+@author: vikram
+"""
+
+# this Is my first code
+print ("hellow world")
+print ('hellop' work')
+print ('hellow world again')
