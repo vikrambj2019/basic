9d9fe27462b980b6341f0c7405b2ab1f63186f35
vikrambj2019 <46285637+vikrambj2019@users.noreply.github.com>
2019-04-09 10:51:32 -0500

    Add files via upload

---
+++ Promotable_Python101/keyFunctions.py
@@ -0,0 +1,150 @@
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import shutil
+import os
+import requests
+import base64
+
+
+# Encode text values to dummy variables(i.e. [1,0,0],[0,1,0],[0,0,1] for red,green,blue)
+def encode_text_dummy(df, name):
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = "{}-{}".format(name, x)
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+# Encode text values to a single dummy variable.  The new columns (which do not replace the old) will have a 1
+# at every location where the original column (name) matches each of the target_values.  One column is added for
+# each target value.
+def encode_text_single_dummy(df, name, target_values):
+    for tv in target_values:
+        l = list(df[name].astype(str))
+        l = [1 if str(x) == str(tv) else 0 for x in l]
+        name2 = "{}-{}".format(name, tv)
+        df[name2] = l
+
+
+# Encode text values to indexes(i.e. [1],[2],[3] for red,green,blue).
+def encode_text_index(df, name):
+    le = preprocessing.LabelEncoder()
+    df[name] = le.fit_transform(df[name])
+    return le.classes_
+
+
+# Encode a numeric column as zscores
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+
+    if sd is None:
+        sd = df[name].std()
+
+    df[name] = (df[name] - mean) / sd
+
+
+# Convert all missing values in the specified column to the median
+def missing_median(df, name):
+    med = df[name].median()
+    df[name] = df[name].fillna(med)
+
+
+# Convert all missing values in the specified column to the default
+def missing_default(df, name, default_value):
+    df[name] = df[name].fillna(default_value)
+
+
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(target_type, '__iter__') else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        dummies = pd.get_dummies(df[target])
+        return df.as_matrix(result).astype(np.float32), dummies.as_matrix().astype(np.float32)
+    else:
+        # Regression
+        return df.as_matrix(result).astype(np.float32), df.as_matrix([target]).astype(np.float32)
+
+
+# Regression Conversion
+def to_xyreg(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(target_type, '__iter__') else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+       # Regression
+    return df.as_matrix(result).astype(np.float32), df.as_matrix([target]).astype(np.float32)
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+
+# Regression chart.
+def chart_regression(pred,y,sort=True):
+    t = pd.DataFrame({'pred' : pred, 'y' : y.flatten()})
+    if sort:
+        t.sort_values(by=['y'],inplace=True)
+    a = plt.plot(t['y'].tolist(),label='expected')
+    b = plt.plot(t['pred'].tolist(),label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+
+# Remove all rows where the specified column is +/- sd standard deviations
+def remove_outliers(df, name, sd):
+    drop_rows = df.index[(np.abs(df[name] - df[name].mean()) >= (sd * df[name].std()))]
+    df.drop(drop_rows, axis=0, inplace=True)
+
+
+# Encode a column to a range between normalized_low and normalized_high.
+def encode_numeric_range(df, name, normalized_low=-1, normalized_high=1,
+                         data_low=None, data_high=None):
+    if data_low is None:
+        data_low = min(df[name])
+        data_high = max(df[name])
+
+    df[name] = ((df[name] - data_low) / (data_high - data_low)) \
+               * (normalized_high - normalized_low) + normalized_low
+        
+# This function submits an assignment.  You can submit an assignment as much as you like, only the final
+# submission counts.  The paramaters are as follows:
+# data - Pandas dataframe output.
+# key - Your student key that was emailed to you.
+# no - The assignment class number, should be 1 through 1.
+# source_file - The full path to your Python or IPYNB file.  This must have "_class1" as part of its name.  
+# .             The number must match your assignment number.  For example "_class2" for class assignment #2.
+def submit(data,key,no,source_file=None):
+    if source_file is None and '__file__' not in globals(): raise Exception('Must specify a filename when a Jupyter notebook.')
+    if source_file is None: source_file = __file__
+    suffix = '_class{}'.format(no)
+    if suffix not in source_file: raise Exception('{} must be part of the filename.'.format(suffix))
+    with open(source_file, "rb") as image_file:
+        encoded_python = base64.b64encode(image_file.read()).decode('ascii')
+    ext = os.path.splitext(source_file)[-1].lower()
+    if ext not in ['.ipynb','.py']: raise Exception("Source file is {} must be .py or .ipynb".format(ext))
+    r = requests.post("https://api.heatonresearch.com/assignment-submit",
+        headers={'x-api-key':key}, json={'csv':base64.b64encode(data.to_csv(index=False).encode('ascii')).decode("ascii"),
+        'assignment': no, 'ext':ext, 'py':encoded_python})
+    if r.status_code == 200:
+        print("Success: {}".format(r.text))
+    else: print("Failure: {}".format(r.text))
+    
